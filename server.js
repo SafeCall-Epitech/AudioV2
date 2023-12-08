623c5d03fd69523ee275d6fcbab2d9dfad78ba01
Nallain <nicolas.allain@epitech.eu>
2023-12-08 19:05:09 +0100

    fix: add more logs (signal data + socket id)

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ const io = require("socket.io")(server, {
 })
 
 io.on("connection", (socket) => {
-    console.log("a user connected")
+    console.log("a user is connected: ", socket.id)
     socket.emit("me", socket.id)
 
     socket.on("disconnect", () => {
-        console.log("a user disconnected")
+        console.log("a user disconnected: ", socket.id)
         socket.broadcast.emit("callEnded")
     })
 
     socket.on("callUser", (data) =>  {
-        console.log("user to call:", data.userToCall)
+        console.log("user to call:", data.userToCall, " signal:", data.signalData.type)
         io.to(data.userToCall).emit("callUser", {signal: data.signalData, from: data.from})
     })
 
     socket.on("answerCall", (data) => {
-        console.log("answer to:", data.to)
+        console.log("answer to:", data.to, " signal:", data.signal.type)
         io.to(data.to).emit("callAccepted", data.signal)
     })
 })
